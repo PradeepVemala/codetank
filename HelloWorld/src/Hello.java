1775df99b2bf86e6e5fb77c5c6f0f5dc9277daac
Pradeep Kumar Vemala <vpradeepkumar@pkrmbendc.com>
2020-06-06 15:45:33 +0530

    code commit-initial-simple interest

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,23 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		int a= 6;
+		float b = (float) 5.2;
+		char C ='a';
+		double d = 78.86543468798; 
+		String name = "Pradeep";
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(C);
+		System.out.println(d);
+		System.out.println(name);
+		
+
+	}
+
+}
+
---
+++ HelloWorld/src/Simpleinterest.java
@@ -0,0 +1,20 @@
+
+public class Simpleinterest {
+
+	public static void main(String[] args) {
+
+		double SI;
+		int p= 10000;
+		double t = 2.5;
+		double r = 11.5;
+		
+	SI=(p*t*r/100);
+	
+System.out.println("The Simple interest on the principle taken would be "+SI);
+		
+double amount= SI+p;
+
+System.out.println("The total amount I would receive is "+amount);
+	}
+
+}
